3178bfd4db14aca93aa11696b1775f39da5793a1
Patrick Carter <xCarter93@gmail.com>
2024-02-02 22:13:30 -0500

    Add experimental configurations for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    esmExternals: "loose",
+    serverMinification: false,
+  },
   images: {
     remotePatterns: [
       {
