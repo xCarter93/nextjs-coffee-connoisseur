ed57a15db05268d523e2754d0b9d3bb917fcad3c
Patrick Carter <xCarter93@gmail.com>
2024-02-02 22:38:39 -0500

    Refactor CoffeeStoreCard component

--- src/components/CoffeeStoreCard.server.tsx
+++ src/components/CoffeeStoreCard.server.tsx
@@ -11,17 +11,19 @@ interface CoffeeStoreCardProps {
 export default function CoffeeStoreCard({ coffeeStore }: CoffeeStoreCardProps) {
   return (
     <Link href={coffeeStore.href}>
-      <Card className="min-h-[200px] bg-white/45 backdrop-blur-sm hover:bg-gray-950">
+      <Card className="min-h-[200px] bg-white/45 px-4 pb-4 backdrop-blur-sm hover:bg-gray-950">
         <CardHeader className="flex items-center">
           <CardTitle>{coffeeStore.name}</CardTitle>
         </CardHeader>
-        <CardContent className="flex items-center justify-center">
+        <CardContent className="relative flex h-48 w-full items-center justify-center">
           <Image
             src={coffeeStore.imageUrl}
             alt="Coffee Store Image"
-            width={260}
-            height={160}
-            className="max-h-[200px] min-h-[200px] rounded-md border-2 border-gray-200 bg-white shadow-lg"
+            layout="fill"
+            objectFit="cover"
+            // width={260}
+            // height={160}
+            className="rounded-md border-2 border-gray-200 bg-white shadow-lg"
           />
         </CardContent>
       </Card>
