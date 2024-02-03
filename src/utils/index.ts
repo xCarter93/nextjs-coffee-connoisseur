a07e4999cb4b82a2bc4afbbae456d37706527ea6
Patrick Carter <xCarter93@gmail.com>
2024-02-02 21:24:16 -0500

    Update production URL in getDomain function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 export const getDomain = () => {
   return new URL(
     process.env.NODE_ENV === "production"
-      ? "https://coffee-connoisseur.vercel.app"
+      ? "https://nextjs-coffee-connoisseur.vercel.app"
       : "http://localhost:3000",
   );
 };
